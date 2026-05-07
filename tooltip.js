22c58326eeb57fa79daa68ec8c0655bbfac8ee59
Amanda <amanda.winkler@penske.com>
2026-05-07 17:36:38 -0400

    progress

--- tooltip.js
+++ tooltip.js
@@ -1,7 +1,30 @@
 class Tooltip extends HTMLElement {
     constructor() {
-        console.log("it is working");
-                }
+        super();
+        this._tooltipContainer;
+        this._tooltipText = "dummy text for tooltip";
+    }
+
+    connectedCallback() {
+        if(this.hasAttribute('text')) {
+            this._tooltipText = this.getAttribute('text'); 
+        }
+        const tooltipIcon = document.createElement("span");
+        tooltipIcon.textContent = ' (?)';
+        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+        this.appendChild(tooltipIcon);
+    }
+
+    _showTooltip() {
+        this._tooltipContainer = document.createElement("div");
+        this._tooltipContainer.textContent = this._tooltipText;
+        this.appendChild(this._tooltipContainer);
+    }
+
+    _hideTooltip() {
+        this.removeChild(this._tooltipContainer);
+    }
 }
 
 customElements.define("my-tooltip", Tooltip);
